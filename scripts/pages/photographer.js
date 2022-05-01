a865b413ba6203a7cc293b4fca50c129127fab0e
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-05-01 14:46:53 +0200

    Put index.js functions in photographer.js | Prepare photographer.html template

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,23 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographerById(id) {
+    let rep = await fetch('data/photographers.json' + id, { method: 'GET' });
+    let response = await rep.json();
+    return response;
+}
+
+async function displayData(photographer) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographerById(id);
+    displayData(photographers);
+};
+
+init();
